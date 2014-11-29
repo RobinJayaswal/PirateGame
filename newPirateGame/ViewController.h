f108c199134138d54b5b8a86f99242fe1ddabb76
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2014-11-29 12:25:02 -0800

    Initial Commit

---
+++ newPirateGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newPirateGame
+//
+//  Created by RobinJayaswal on 11/29/14.
+//  Copyright (c) 2014 RobinJayaswal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ newPirateGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  newPirateGame
+//
+//  Created by RobinJayaswal on 11/29/14.
+//  Copyright (c) 2014 RobinJayaswal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
